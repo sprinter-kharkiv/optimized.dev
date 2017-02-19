a12138ef38fafe248d270cde5d2c35a0acfe8bdc
sprinter.kharkiv <sprinter.kharkiv@gmail.com>
2017-02-19 20:53:06 +0200

    desc

--- app/index.php
+++ app/index.php
@@ -1,7 +1,35 @@
 <?php
 require "header.php";
-require "sidebar.php";
 
+/*--------------------
+    login / logout
+--------------------*/
+
+$user = isset($_GET['user_name']) ? $_GET['user_name'] : null;
+$logout = isset($_GET['logout']) ? $_GET['logout'] : null;
+
+if ($user) {
+    file_put_contents('user.txt', $user);
+} elseif ($logout === 'yes') {
+    file_put_contents('user.txt', '');
+}
+$current_user = file_get_contents('user.txt');
+
+
+if (isset($current_user) && $current_user !== '') {
+    $logout_hidd = 'visible';
+    $login_hidd = 'hidden';
+    $post_access = '';
+} else {
+    $logout_hidd = 'hidden';
+    $login_hidd = 'visible';
+    $post_access = 'disabled';
+};
+
+require "sidebar.php";
+/*--------------------
+    posts
+--------------------*/
 $data_base = "DB.txt";
 $current_posts = array();
 if (file_get_contents($data_base) != "") {
@@ -13,7 +41,7 @@ if (!empty($_GET['post_title']) && !empty($_GET['post_text'])) {
     $new_post = array(
         'title' => htmlspecialchars($_GET['post_title']),
         'text' => htmlspecialchars($_GET['post_text']),
-        'author' => htmlspecialchars($_GET['post_title'])
+        'author' => $current_user,
     );
 };
 $new_post_title = '';
@@ -21,36 +49,35 @@ $new_post_title = $new_post['title'];
 
 $refresh = array();
 foreach ($current_posts as $key => $val) {
-    if (in_array($new_post_title,$val)) {
+    if (in_array($new_post_title, $val)) {
         $refresh[] = 0;
-    }else {
+    } else {
         $refresh[] = 1;
     }
 };
 
-if (!in_array(0,$refresh) && $new_post_title != '') {
+if (!in_array(0, $refresh) && $new_post_title != '') {
     $current_posts[] = $new_post;
     $json_date = json_encode($current_posts);
     file_put_contents($data_base, $json_date);
 };
 
 
-
-
 $count_posts = count($current_posts);
 ?>
 <pre>
     <?php
-    echo count($current_posts);
+    //echo count($current_posts);
+    echo $current_user;
 
     ?>
 </pre>
 <div class="content">
-    <div>
+    <div class="posts_block">
         <form method="GET" class="add_posts_form">
             <input type="text" name="post_title">
             <textarea name="post_text"></textarea>
-            <input type="submit" value="add post">
+            <input type="submit" value="add post" <?= $post_access ?>>
         </form>
     </div>
     <?php for ($i = 0; $i < $count_posts; $i++) {
--- app/sidebar.php
+++ app/sidebar.php
@@ -1,5 +1,28 @@
 <section class="sidebar">
-    <ul>
+    <div class="login_form">
+        <form action="" method="get" class="<?= $login_hidd ?>">
+            <fieldset>
+                <input type="text" name="user_name">
+                <input type="password" name="user_password">
+            </fieldset>
+            <input type="submit" value="login">
+        </form>
+        <h3><?php
+            if (isset($current_user) && $current_user !== '') {
+                echo "Login as:" . $current_user;
+            } else {
+                echo '<a href="">link for registration</a>';
+            };
+            ?></h3>
+
+        <form action="" method="get">
+            <input type="text" name="logout" value="yes" hidden>
+            <input type="submit" value="logout" class="<?= $logout_hidd ?>">
+        </form>
+
+
+    </div>
+    <ul class="sidebar_menu">
         <li>menu item 1</li>
         <li>menu item 2</li>
         <li>menu item 3</li>
